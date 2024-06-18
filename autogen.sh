f1aee0321f80a6c82cc73fac11c6b187cc6bea6d
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-06-18 08:59:04 +0200

    check windows

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # (Re)Generate configure script and all build related files for i18n
 
-autoreconf -W portability -vism --force >/dev/null
+autoreconf -W portability -vism --force >/dev/null --verbose
 
